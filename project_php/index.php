71cb80c9a1b4bc79a0d6aec34ad517ba57d4597e
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-01 17:33:20 +0100

    Created file in project Vue + style

--- project_php/index.php
+++ project_php/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include __DIR__ . '/../api/api.php';
+include __DIR__ . '/../api/data.php';
 
 ?>
 
---
+++ project_vue/script.js
@@ -0,0 +1,10 @@
+console.log('Vue ok', Vue);
+
+Vue.config.devtools = true;
+
+const app = new Vue({
+   el: '#app',
+   data: {
+      movies: [],
+   }
+});

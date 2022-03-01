bc8f9161e2a3592e0ed737ddaff72b766ebf9fbb
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-01 17:51:12 +0100

    Created file project vue

--- project_vue/script.js
+++ project_vue/script.js
@@ -3,8 +3,18 @@ console.log('Vue ok', Vue);
 Vue.config.devtools = true;
 
 const app = new Vue({
-   el: '#app',
+   el: '#root',
    data: {
       movies: [],
+   },
+   methods: {
+      getApi() {
+         axios.get('http://localhost:8888/php-ajax-dischi/api/index.php').then(res => {
+            this.movies = res.data;
+         })
+      }
+   },
+   mounted() {
+      this.getApi;
    }
 });
